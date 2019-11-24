dd8eb3b108dcd0e3fd69e599eb5aebca5823e514
Kiryl Stasevich <kirylstasevich@gmail.com>
2019-11-24 10:49:58 +0300

    init

---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# Microservices
+
+The problems of microservice architecture:
+1) Communication
+2) Authorization
+3) Continous delivery
+4) How to setup it fast in development (I guess package everything in docker containers)
+5) Microservice detection
---
+++ microservices/kitchen/kitchen.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({
+  path: path.resolve(__dirname, '.env'),
+});
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Kitchen app listening on port ${PORT}!`);
+});
