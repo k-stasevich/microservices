d62dfbea4ee4209c70ded98130de278e12e7f6f4
Kiryl Stasevich <kirylstasevich@gmail.com>
2019-11-24 11:21:12 +0300

    feat: dotenv config is migrated to a separate file

--- microservices/delivery/delivery.js
+++ microservices/delivery/delivery.js
@@ -1,9 +1,4 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-dotenv.config({
-  path: path.resolve(__dirname, '.env'),
-});
 const app = express();
 
 app.get('/', function(req, res) {
